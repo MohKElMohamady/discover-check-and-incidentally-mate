561053822029c8c1591260071900c39e2cfe5d2a
mohkelmohamady <moh.k.elmohamady@gmail.com>
2022-07-16 23:48:47 +0200

    Added the king piece class

---
+++ src/main/java/com/discover/check/and/incidentally/mate/pieces/King.java
@@ -0,0 +1,58 @@
+package com.discover.check.and.incidentally.mate.pieces;
+
+import com.discover.check.and.incidentally.mate.board.Board;
+import com.discover.check.and.incidentally.mate.board.BoardUtils;
+import com.discover.check.and.incidentally.mate.board.Tile;
+import com.discover.check.and.incidentally.mate.board.moves.AttackMove;
+import com.discover.check.and.incidentally.mate.board.moves.MajorMove;
+import com.discover.check.and.incidentally.mate.board.moves.Move;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class King extends Piece{
+
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {-9, -8, -7, -1, 1, 7, 8, 9};
+
+    public King(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE){
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
+
+            if(isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+               isEighthExclusion(this.piecePosition, currentCandidateOffset)){
+                continue;
+            }
+
+            if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                if(!candidateDestinationTile.isTileOccupied()){
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }else{
+                    final Piece pieceOnOccupiedTile = board.getTile(candidateDestinationCoordinate).getPiece();
+                    final Alliance pieceAlliance = pieceOnOccupiedTile.getPieceAlliance();
+                    if(this.pieceAlliance != pieceAlliance){
+                        legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceOnOccupiedTile));
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == -1 ||
+                candidateOffset == 7);
+    }
+
+    private static boolean isEighthExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 1 ||
+                candidateOffset == 9);
+    }
+}
